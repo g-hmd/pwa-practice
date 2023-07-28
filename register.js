fa514f18bca3837fbb9e68b0963ce3e164deebae
g-hmd <140684913+g-hmd@users.noreply.github.com>
2023-07-28 16:17:43 +0900

    Update register.js

--- register.js
+++ register.js
@@ -1,13 +1,3 @@
-
-/**
- * 画面タッチでカラフルな線を描く
- * ------------------------------
- */
-var drawing = false
-var last_x = null
-var last_y = null
-var canvas = document.getElementById('stage')
-var ctx = canvas.getContext('2d')
 if ('serviceWorker' in navigator) {
 	navigator.serviceWorker.register('service_worker.js').then(function(registration) {
 		console.log('ServiceWorker registration successful with scope: ', registration.scope);
@@ -16,48 +6,17 @@ if ('serviceWorker' in navigator) {
 	});
 }
 
-function resize() {
-	canvas.setAttribute('width', window.innerWidth*2)
-	canvas.setAttribute('height', window.innerHeight*2)
-	ctx.font = '30px serif'
-	ctx.fillText('PWAサンプルアプリ', 20, 40)
-	ctx.font = '25px serif'
-	ctx.fillText('マウスや指タッチで線が描けるよ！', 15, 80)
-	ctx.lineWidth = 5
-	ctx.scale(2, 2)
+function onlinecheck() {
+    if (window.navigator.onLine) {
+        console.log("online");
+        alert('オンライン');
+    } else {
+        console.log("offline");
+        alert('オフライン');
+    }
 }
-resize()
-window.addEventListener('resize', resize)
-window.addEventListener('orientationchange', resize)
-canvas.addEventListener('mousedown', drawStart, false)
-canvas.addEventListener('touchstart', drawStart, false)
-function drawStart(event) {
-	event.preventDefault()
-	drawing = true
-	last_x = event.pageX
-	last_y = event.pageY
-}
-canvas.addEventListener('mousemove', drawLine, false)
-canvas.addEventListener('touchmove', drawLine, false)
-function drawLine(event) {
-	if(!drawing) return
-	if(event.type==='touchmove') event = event.changedTouches[0]
-	ctx.strokeStyle =
-		'rgb('+
-		(Math.floor(Math.random()*255)) + ',' +
-		(Math.floor(Math.random()*255)) + ',' +
-		(Math.floor(Math.random()*255)) + ')'
-	;
-	ctx.beginPath()
-	ctx.moveTo(last_x, last_y)
-	ctx.lineTo(event.pageX, event.pageY)
-	ctx.stroke()
-	ctx.closePath()
-	last_x = event.pageX
-	last_y = event.pageY
-}
-canvas.addEventListener('mouseup', drawFinish, false)
-canvas.addEventListener('touchend', drawFinish, false)
-function drawFinish() {
-	drawing = false
+
+function screenSize() {
+    alert("画面の高さ"&screen.height);
+    alert("画面の幅"&screen.width);
 }
